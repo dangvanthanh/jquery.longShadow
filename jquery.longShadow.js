0bec2174b3de43b584e921db4c313a556a4a0d39
Dang Van Thanh <dangvanthanh189@gmail.com>
2013-08-30 07:57:21 +0700

    Long Shadow jQuery Plugin

---
+++ jquery.longShadow.js
@@ -0,0 +1,84 @@
+/*
+* LongShadow jQuery Plugin
+* Copyright (c) 2013 Dang Van Thanh
+* Version: 1.0.0
+* Source: git://github.com/dangvanthanh/jquery.longShadow.git
+* Licensed under MIT 
+*/
+;(function($, window, document, undefined) {
+
+	// Name Long Shadow Query Plugin and Parameters
+	var pluginName = 'longShadow',
+			defaults = {
+				colorShadow: '#ccc',
+				sizeShadow: 50,
+				directionShadow: '' // Default bottom-right
+			};
+
+	// Long Shadow Plugin
+	function Plugin(element, options) {
+		this.element = element;
+
+		this.options = $.extend({}, defaults, options);
+
+		this._defaults = defaults;
+		this._name = pluginName;
+
+		this._shadow = '';
+
+		this.init();
+	}
+
+	Plugin.prototype = {
+		init: function() {
+			var textshadow = '',
+					color = this.options.colorShadow;
+
+			for (var i = 0; i < this.options.sizeShadow; i++) {
+				var opposite = i * -1;
+				switch (this.options.directionShadow) {
+					case 'top':
+						textshadow += '0 ' + opposite + 'px 0' + color + ',';
+						break;
+					case 'right':
+						textshadow += i + 'px 0 0 ' + color + ',';
+						break;
+					case 'bottom':
+						textshadow += '0 ' + i + 'px 0 ' + color + ',';
+						break;
+					case 'left':
+						textshadow += opposite + 'px ' + '0 0 ' + color + ',';
+						break;
+					case 'top-left':
+						textshadow += opposite + 'px ' + opposite + 'px 0 ' + color + ',';
+						break;
+					case 'top-right':
+						textshadow += i + 'px ' + opposite + 'px 0 ' + color + ',';
+						break;
+					case 'bottom-left':
+						textshadow += opposite + 'px ' + i + 'px 0 ' + color + ',';
+						break;
+					case 'bottom-right':
+						textshadow += i + 'px ' + i + 'px 0 ' + color + ',';
+						break;
+					default:
+						textshadow += i + 'px ' + i + 'px 0 ' + color + ',';
+						break;
+				}
+			}
+
+			this._shadow = textshadow.slice(0, -1);
+
+			this.element.style.textShadow = this._shadow;
+		}
+	};
+
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+			}
+		});
+	};
+
+})(jQuery, window, document);
